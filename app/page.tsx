95115735414b073c26d634a323833ac8a1265cb9
Byron Panimboza <byron.115@hotmail.com>
2025-07-28 12:34:46 +0200

    refactor: remove unused state from Home component and simplify HeaderDesktop usage

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,17 @@
 import HeaderDesktop from "../components/organisms/HeaderDesktop";
 import HeaderMobile from "../components/organisms/HeaderMobile";
 import HeroSection from "@/components/organisms/HeroSection";
-import { useState } from "react";
 import ProductSection from "@/components/organisms/ProductSection";
 import ClientSection from "@/components/organisms/ClientSection";
 import Footer from "@/components/organisms/Footer";
 
 export default function Home() {
-  const [headerOffset, setHeaderOffset] = useState(0);
+  
 
   return (
     <>
       <div className="flex flex-col gap-72">
-        <HeaderDesktop setHeaderOffset={setHeaderOffset} />
+        <HeaderDesktop />
         <HeaderMobile />
         <HeroSection />
         <ClientSection />
